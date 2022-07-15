3c535a37c31448fdd4739195c617d52e85d1adec
Winnie-Waiguru <winniewaiguru@gmail.com>
2022-07-15 11:20:58 +0300

    if statement change

--- objects.js
+++ objects.js
@@ -1,10 +1,14 @@
-let fruit= {
-    fruitPriceList:{"Orange": 30, "mango": 15, "avocado": 40},
-    calculateFruitCost:function(fruitName,quantity){
-        return `${quantity} ${fruitName} at ${quantity * this.fruitPriceList[fruitName]}`
-        }
-}
-console.log(fruit.calculateFruitCost("avocado",3))
+// let fruit= {
+//     fruitPriceList:{"Orange": 30, "mango": 15, "avocado": 40},
+//     calculateFruitCost:function(fruitName,quantity){
+//         if (this.fruitPriceList[fruitName]===undefined){
+//             return `${fruitName} is not available`
+//         }
+//         return `${quantity} ${fruitName} at ${quantity * this.fruitPriceList[fruitName]}`
+//         }
+// }
+// console.log(fruit.calculateFruitCost("apple",3))
+
 
 class KioskCalc{
     constructor(fruit,quantity){
@@ -12,6 +16,9 @@ class KioskCalc{
         this.quantity=quantity
         this.fruitPriceList={"orange": 30, "mango": 15, "avocado": 40}
         this.totalCost=function(){
+            if (this.fruitPriceList[this.fruit]===undefined){
+                return `${this.fruit} is not a available.`
+            }
               return `${this.quantity} ${this.fruit} at KES ${this.fruitPriceList [this.fruit]* this.quantity}`
         }
 
@@ -19,6 +26,6 @@ class KioskCalc{
     }
 
 }
-var kioskCalc =new KioskCalc("mango",2)
 
+var kioskCalc =new KioskCalc("pineapple",2)
 console.log(kioskCalc.totalCost())
